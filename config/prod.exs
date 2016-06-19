1805508bccc4f7861cfa587b218b5b2e03929949
Dinesh Vitharanage <dinesh.vitharanage@myjobmatcher.com>
2016-06-19 15:28:52 +0100

    Fixed typo in config

--- config/prod.exs
+++ config/prod.exs
@@ -30,7 +30,7 @@ config :chat, Chat.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
-config :chat Chat.Repo,
+config :chat, Chat.Repo,
   adapter: Ecto.Adaptors.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
